e6e32296e298043c9b1d09a5f7317257cc8b3588
SuBin Cho <sbjo7713@gmail.com>
2025-07-27 22:56:39 +0900

    initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/traffic/trafficboard/TrafficBoardApplication.java
@@ -0,0 +1,13 @@
+package traffic.trafficboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/traffic/trafficboard/TrafficBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package traffic.trafficboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
